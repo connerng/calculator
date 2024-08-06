75df71220217926959ea150fd72f0c19343adc18
connerng <cjngadisastra@gmail.com>
2024-08-06 12:15:17 -0700

    Fully functional

--- main.js
+++ main.js
@@ -7,60 +7,31 @@ function isOperator(str) {
     }
 }
 
-function precedence(operator) {
-    if (operator == "*" || operator == "/") {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
 function numberClicked(num) {
-    if (document.getElementById("ans-text").innerHTML == "0") {
-        document.getElementById("ans-text").innerHTML = num;
+    if (document.getElementById("ans-display").value == "") {
+        document.getElementById("ans-display").value = num;
     } else {
-        document.getElementById("ans-text").innerHTML += num;
+        document.getElementById("ans-display").value += num;
     }
 }
 
 function backspace() {
-    document.getElementById("ans-text").innerHTML = document.getElementById("ans-text").innerHTML.slice(0, -1);
+    document.getElementById("ans-display").value = document.getElementById("ans-display").value.slice(0, -1);
 }
 
 function clearAns() {
-    document.getElementById("ans-text").innerHTML = "0";
+    document.getElementById("ans-display").value = "";
 }
 
 function operatorClicked(op) {
-    if (!isOperator(document.getElementById("ans-text").innerHTML.slice(-1))) {
-        document.getElementById("ans-text").innerHTML += op;
+    if (!isOperator(document.getElementById("ans-display").value.slice(-1))) {
+        document.getElementById("ans-display").value += op;
     }
 }
 
-function evaluate() {
-    expression = document.getElementById("ans-text").innerHTML;
-    if (!isOperator(expression.slice(-1))) {
-        const itemList = [];
-        curItem = "";
-        for (let i = 0; i < expression.length(); i++) {
-            if (isOperator(expression.slice(i, i+1))) {
-                itemList.push(curItem);
-                curItem = "";
-                itemList.push(expression.slice(i, i+1));
-            } else {
-                curItem += expression.slice(i, i+1);
-            }
-        }
-
-        stack = [];
-        postfix = [];
-        for (let i = 0; i < itemList.length(); i++) {
-            cur = itemList[i];
-            if (!isOperator(cur)) {
-                postfix.push(cur);
-            } else {
-                
-            }
-        }
-    }
+function evaluateExpression() {
+    const expression = document.getElementById("ans-display").value
+    document.getElementById("ans-display").value = eval(expression);
 }
+
+
